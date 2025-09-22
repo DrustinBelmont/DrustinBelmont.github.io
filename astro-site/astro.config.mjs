1839cbd0a0990c960058a8510b3953cd25d89a8f
Berin <berin358@outlook.fr>
2025-09-22 09:27:58 +0200

    delete

--- astro-site/astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://drustinbelmont.github.io', // ton URL finale
-  outDir: '../dist' // (optionnel : tu peux générer dans ../dist pour déploiement direct)
-});
