961a282c42b313f9c3a20201c123181d5094f466
Berin <berin358@outlook.fr>
2025-09-22 10:06:21 +0200

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://drustinbelmont.github.io/',
+  site: 'https://drustinbelmont.github.io',
   base: '/',
 });
